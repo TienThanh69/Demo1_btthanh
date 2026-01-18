018eb783dcd9008d13f480dcf19de072dbaa921c
Hoang de <apksuyuki1@gmail.com>
2026-01-18 23:46:57 +0700

    Hoàn thành Part 1: Di chuyển máy bay và hiệu ứng lửa

---
+++ Assets/Blinking.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Blinking : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // Lấy thành phần SpriteRenderer từ đối tượng
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Đảo ngược trạng thái hiển thị (Bật -> Tắt, Tắt -> Bật)
+        spriteRenderer.enabled = !spriteRenderer.enabled;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        // Dòng này sẽ in tọa độ chuột liên tục ra Console để bạn kiểm tra
+        Debug.Log("Toạ độ chuột: " + mousePos);
+
+        mousePos.z = 10f;
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPoint.z = 0;
+        transform.position = worldPoint;
+    }
+}
